085843df64cb4ce4d91b44811557210d9819cfc6
lizhigaomac <lizhigao@021.com>
2018-05-16 17:37:23 +0800

    update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,46 @@
-const path = require('path')
+const webpack = require('webpack');
+const path = require('path');
+const libName = 'ShfeLib';
+const plugins = [];
+const outputFile = libName + '.js';
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'shfelib.js',
-        path: path.resolve(__dirname, 'dist')
-    }
+const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+const env = process.env.WEBPACK_ENV;
+
+if (env === 'build') {
+  plugins.push(new UglifyJsPlugin({ minimize: true }));
+  outputFile = libName + '.min.js';
 }
 
+const config = {
+  entry: __dirname + '/src/index.js',
+  devtool: 'source-map',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: outputFile,
+    library: libName,
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  module: {
+    loaders: [
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: 'babel',
+        exclude: /node_modules/
+      },
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    root: path.resolve('./src'),
+    extensions: ['', '.js']
+  },
+  plugins: plugins
+};
+
+module.exports = config;
